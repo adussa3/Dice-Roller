5fc2a7f0384c1e65c1f0b815e54da5834446b603
Anunoy Dussa <adussa3@gmail.com>
2021-05-30 22:55:06 -0500

    Removed toast and updated text
    1) Removed the toast and replaced it with the rollDice() method
    2) Created the rollDice() method which updated the text to a random number between 1 to 6

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,19 @@ class MainActivity : AppCompatActivity() {
         // Get the "Roll" button from the layout
         val rollButton: Button = findViewById(R.id.roll_button)
 
-        // When the "Roll" button is clicked, show a toast
+        // When the "Roll" button is clicked, update the text to a random number between 1 to 6
         rollButton.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    /**
+     * Updated the text to a random number between 1 to 6
+     */
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
