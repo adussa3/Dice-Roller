70abeb77256730ea14f37fe8ca758cfabcda2381
Anunoy Dussa <adussa3@gmail.com>
2021-05-31 11:17:43 -0500

    Replaced text with dice images
    1) Removed number text from the MainActivity and it's Layout
    2) Replaced the number text with dice images

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -21,12 +22,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Updated the text to a random number between 1 to 6
+     * Updated the image to a dice roll image between 1 to 6
      */
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random.nextInt(6) + 1
 
-        resultText.text = randomInt.toString()
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
