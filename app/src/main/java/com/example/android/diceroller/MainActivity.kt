b801189d8929ca43b411eb694c8ed2c28140c0c5
Anunoy Dussa <adussa3@gmail.com>
2021-05-30 22:41:19 -0500

    Made a toast appear when "Roll" button is clicked
    I retrieved the "Roll" button from the MainActivity's Layout and showed a toast when it was clicked

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the "Roll" button from the layout
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        // When the "Roll" button is clicked, show a toast
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
