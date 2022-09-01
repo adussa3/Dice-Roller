867ce4fdd6d7a073ef51bfdedb4166829870dd17
Anunoy Dussa <adussa3@gmail.com>
2022-08-31 23:15:45 -0500

    Updated rollDice() to show dice image on button click

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,19 +3,20 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var resultTextView: TextView
+    lateinit var diceImageView: ImageView
     lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultTextView = findViewById(R.id.result_text_view)
+        diceImageView = findViewById(R.id.dice_image_view)
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -25,6 +26,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
-        resultTextView.text = randomInt.toString()
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImageView.setImageResource(drawableResource)
     }
 }
