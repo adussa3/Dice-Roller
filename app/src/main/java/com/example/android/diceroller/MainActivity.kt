27d4a7adc013e0a3ae6e8579188b9e2f2dc773fe
Anunoy Dussa <adussa3@gmail.com>
2022-08-31 22:51:57 -0500

    Updated TextView to display a random number on button click

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,18 +3,28 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var resultTextView: TextView
     lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultTextView = findViewById(R.id.result_text_view)
         rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
+            rollDice()
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        resultTextView.text = randomInt.toString()
     }
 }
